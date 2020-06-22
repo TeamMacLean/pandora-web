103d71a826f6f6ec67b54f68378f4696d2f9772c
George Deeks <george@neontribe.co.uk>
2020-06-22 15:28:30 +0100

    feat: sequence

---
+++ components/SequenceCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="field">
+    <b-tag 
+        close-type='is-danger'
+        attached
+        closable
+        aria-close-label="Close tag"
+        @close="handleDelete"
+      >
+        {{sequence.name}}
+    </b-tag>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ["sequence"],
+  methods: {
+    handleDelete() {
+      this.$emit('deactivateSequence', this.sequence.id);
+    }
+  }
+};
+
+</script>
+
+<style scoped>
+
+</style>
--- store/index.js
+++ store/index.js
@@ -3,7 +3,8 @@ import uuid from "uuid/v1";
 export const state = () => ({
     user: null,//{ username: 'pagem' },
     boxes: [],
-    bags: []
+    bags: [],
+    sequences: [],
 })
 
 export const mutations = {
@@ -21,6 +22,16 @@ export const mutations = {
     },
     addBagToState(state, bag) {
         state.bags.push(bag)
+    },
+    setSequencesState(state, sequences) {
+        state.sequences = sequences;
+    },
+    addSequenceToState(state, sequence) {
+        state.sequences.push(sequence)
+    },
+    updateSequenceStatusInState(state, { modifiedSequenceId, newIsActiveValue }) {
+        const targetIndex = state.sequences.findIndex(seq => seq.id === modifiedSequenceId);
+        state.sequences[targetIndex].isActive = newIsActiveValue;
     }
 }
 
@@ -46,10 +57,36 @@ export const actions = {
                 console.error(err);
             })
     },
-    // async addBag({ commit }) {
-    //     // return commit
-    // },
-    // async refreshBags({ commit }) {
 
-    // }
+    async addSequence({ commit }) {
+        return this.$axios.get('/sequences/new')
+            .then(({ data }) => {
+                commit('addSequenceToState', data.sequence);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    },
+    async refreshSequences({ commit }) {
+        return this.$axios.get('/sequences')
+            .then(({ data }) => {
+                commit('setSequencesState', data.sequences)
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    },
+    async toggleActivateSequence(state, {sequenceId, newStatus}) {
+        return this.$axios.put('/sequence/', {
+            id: sequenceId,
+            isActive: newStatus,
+        }).then(function (response) {
+            state.commit('updateSequenceStatusInState', {
+                modifiedSequenceId: response.data.id, 
+                newIsActiveValue: response.data.isActive, 
+            })
+        }).catch(function (error) {
+            console.log(error)
+        })
+    }
 }
